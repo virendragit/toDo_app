240daf76cb57e3cc8a224413d429bd09a5af8948
Virendra Gupta <vgupta@ivycomptech.com>
2021-04-02 18:37:48 +0530

    Initial Commit

---
+++ ToDo_Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo_Task
+//
+//  Created by Virendra Gupta on 4/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ToDo_TaskTests/ToDo_TaskTests.swift
@@ -0,0 +1,33 @@
+//
+//  ToDo_TaskTests.swift
+//  ToDo_TaskTests
+//
+//  Created by Virendra Gupta on 4/2/21.
+//
+
+import XCTest
+@testable import ToDo_Task
+
+class ToDo_TaskTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
