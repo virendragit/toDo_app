58a6314436a12a9af59f5e14ae79aa7c82acb1de
Virendra Gupta <vgupta@ivycomptech.com>
2021-04-04 02:11:16 +0530

    first commit

---
+++ ToDo_Task/TaskViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TaskViewController.swift
+//  ToDo_Task
+//
+//  Created by Virendra Gupta on 4/2/21.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
